0b9b3e2c4dae7ef44508c61997b2d573ab21b5b0
tristanbay <85427980+tristanbay@users.noreply.github.com>
2026-02-04 21:32:54 -0800

    Make program display global equave, add note names and note colors to test scale file

--- include/output.h
+++ include/output.h
@@ -6,15 +6,15 @@
 void print_note(myri_note_t data)
 { // function for printing each note
 	printf("%-4ld\\%-4lu<%-3ld/%-3lu>\t%-10s\t#%02x%02x%02x\n",
-		data.ed.n, data.ed.d, data.ji.n, data.ji.d, data.name,
-		data.color[0], data.color[1], data.color[2]);
+		data.ed.n, data.ed.d, data.ji.n, data.ji.d, data.name, data.color[0], data.color[1], data.color[2]);
 }
 
 void print_scale(myri_scale_t scale)
 { // display contents of scale data structure
 	printf("%s\n\n", scale.title);
-	printf("Notes per channel: %u\nChannels used:     %u\n\n",
-		scale.used_notes, scale.used_channels);
+	printf("Notes per channel: %u\nChannels used:     %u\n", scale.used_notes, scale.used_channels);
+	printf("Global equave: %-4ld\\%-4lu<%-3ld/%-3lu>\n\n",
+		scale.equave_ed.n, scale.equave_ed.d, scale.equave_ji.n, scale.equave_ji.d);
 	for (uint8_t i = 0; i < scale.used_channels; ++i) {
 		for (uint8_t j = 0; j < scale.used_notes; ++j) {
 			printf("Ch %02d Nt %03d:\t", i + 1, j + 1);
